b81b22b94fcd51126097b7d738b1811e99d0fd77
johan.ramirez <johan.ramirez@sofka.com.co>
2022-08-27 22:21:33 -0500

    deploy

--- dataBase/config.js
+++ dataBase/config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnection = async () => {
   try {
-    await mongoose.connect(process.env.DB_CONNECTION.toString(), {
+    await mongoose.connect(process.env.DB_CONNECTION, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
